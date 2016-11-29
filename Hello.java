86b1e5eb3aa8b537e641912fe451fd71d406201a
Lee KH <LeeKH@naver.com>
2016-11-29 11:16:56 +0900

    Hello his git

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println*("Hello Git");
-		System.out.println("myGit");
-	
+		System.out.println("Hello Git");
+		System.out.println("my my Git");
+		System.out.println("Hello her Git");
 	
 	}
 }
